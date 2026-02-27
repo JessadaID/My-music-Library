6cf42fe61ca6df4e6e6c6c61ae691fd3fba1f7b6
Jessada <jessada@bananacoding.com>
2026-02-27 14:17:22 +0700

    fix bug responsice

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
     <div>
       <h1 className="text-3xl font-bold mb-4">YouTube Music Player</h1>
 
-      <section>
-        <div className="grid gap-4">
-          <div className="col-span-3 h-full ">
+      <section className="min-w-0">
+        <div className="grid gap-4 min-w-0">
+          <div className="col-span-3 h-full min-w-0">
             <MusicPlayer />
           </div>
         </div>
